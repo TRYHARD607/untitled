a003ce3bef9bed8d4eead8be4352a1fea111193e
Alex Kvashnev <brasuni63@gmail.com>
2023-11-03 12:17:34 +0700

    features slice design firs implementation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
 import { Link } from 'react-router-dom';
-import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
-import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
-import { Suspense, useContext, useState } from 'react';
-import { Theme, ThemeContext } from './theme/ThemeContext';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { Suspense } from 'react';
+import { classNames } from 'shared/lib/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -17,8 +16,8 @@ export const App = () => {
       <Link to='/about'>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/about' element={<AboutPageLazy />} />
-          <Route path='/' element={<MainPageLazy />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/' element={<MainPage />} />
         </Routes>{' '}
       </Suspense>
     </div>
