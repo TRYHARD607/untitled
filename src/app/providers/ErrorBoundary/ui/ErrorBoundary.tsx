852f020f131ecfbcc032bf9adf56cc4ac07e2bf9
Alex Kvashnev <brasuni63@gmail.com>
2023-11-21 18:01:04 +0700

    add error boundary

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/indent */
+import React, { type ErrorInfo, type ReactNode, Suspense } from 'react';
+import { PageError } from 'widgets/PageError';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    if (error) {
+      console.error(error);
+    }
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+    if (hasError) {
+      return (
+        <Suspense fallback=''>
+          <PageError />
+        </Suspense>
+      );
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import 'shared/config/i18n/i18n';
 
 import { App } from 'app/App';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,9 +9,11 @@ import { BrowserRouter } from 'react-router-dom';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
+  <ErrorBoundary>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </ErrorBoundary>
 );
