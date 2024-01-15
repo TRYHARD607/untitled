eeaaa86d6b063e46b34e02159149ee847c06bfe9
Alex Kvashnev <brasuni63@gmail.com>
2024-01-15 14:11:29 +0700

    add profile page screenshots reference

--- src/shared/config/storybook/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator.tsx
@@ -1,10 +1,12 @@
 import { type ReducersMapObject } from '@reduxjs/toolkit';
 import { type StoryFn } from '@storybook/react';
 import { type StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { profileReducer } from 'entities/Profile';
 import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 
 const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
   loginForm: loginReducer,
+  profile: profileReducer,
 };
 
 export const StoreDecorator =
